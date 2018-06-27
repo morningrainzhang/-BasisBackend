3aef783fa479f426bc3a41fbc9ff42e459340578
Morain <799842527@qq.com>
2018-06-27 15:06:34 +0800

    111

---
+++ web/1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@version: ??
+@author: Morain
+@file: 1.py
+@time: 2018/6/27 15:03
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    print(__doc__)
